42ce18e599e1bf87c2948b11d949f941b0ce6204
CJ Maxwell <christopher.j.maxwell.16@gmail.com>
2020-11-08 06:22:44 -0500

    Added documentation + type hints to v2

--- v2.py
+++ v2.py
@@ -1,9 +1,9 @@
-from typing import *
+from typing import List, Dict, Tuple
 
 from gamebuilder import *
 
 
-def open(row: int, column: int) -> Union[bool, int]:
+def open(row: int, column: int) -> int:
     assert 'board' in globals()
     _key: List[List[str]] = [[c for c in _row.split()] for _row in key.splitlines()]
     res: str = _key[row][column]
@@ -12,7 +12,9 @@ def open(row: int, column: int) -> Union[bool, int]:
 
 
 class Gridspace:
-    def __init__(self, r, c, hint, nrows, ncols):
+    """Represents a space on the board which is aware of its neighbors & the # of mines around it."""
+
+    def __init__(self, r: int, c: int, hint: str, nrows: int, ncols: int):
         self.row = r
         self.col = c
         self.hint = hint
@@ -31,26 +33,63 @@ class Gridspace:
         return f'({self.row},{self.col}: {self.hint})'
 
 
-def splitgrid(gridstr):
-    return [row.split() for row in gridstr.splitlines()]
+def boardtohashmap(board_2d: List[List[str]]) -> Dict[Tuple[int, int], Gridspace]:
+    """
+    Creates a dictionary that represent the evolving gameboard during play.
 
+    :param board_2d: a list of lists representing the 2d-board
+    :return: a dictionary that represent the evolving gameboard during play.
+    """
 
-def hashmaptostring(hashmap, nrows, ncols):
-    return "\n".join(" ".join(hashmap[(r, c)].hint for c in range(ncols)) for r in range(nrows))
-
-
-def boardtohashmap(board_2d):
     return {
         (r, c): Gridspace(r, c, board_2d[r][c], len(board_2d), len(board_2d[r]))
         for r in range(len(board_2d)) for c in range(len(board_2d[r]))
     }
 
 
-def solve_mine(map, n):
+def hashmaptostring(hashmap: Dict[Tuple[int, int], Gridspace], nrows: int, ncols: int) -> str:
+    """
+    Get a string representation of the 2d-board state during play.
 
-    def open_zeros(display=False):
-        """Open Zeros"""
-        print(hashmaptostring(lookup, len(board_2d), len(board_2d[0])))
+    :param hashmap: a dictionary representing the 2d-board during play
+    :param nrows: # of rows in the 2d-board
+    :param ncols: # of columns in the 2d-board
+    :return: a string representation of the 2d-board state during play
+    """
+
+    return "\n".join(" ".join(hashmap[(r, c)].hint for c in range(ncols)) for r in range(nrows))
+
+
+def splitgrid(gridstr: str) -> List[List[str]]:
+    """
+    Splits a string representation of a 2d-board into a 2d-list. (splits on whitespace & newlines)
+
+    :param gridstr: a string representation of a 2d-board
+    :return: a 2d-list representing a 2d-board
+    """
+
+    return [row.split() for row in gridstr.splitlines()]
+
+
+def solve_mine(map: str, n: int) -> str:
+    """
+    Plays a game of Minesweeper as far as possible without making a guess. (Kata mandated entry point for program.)
+
+    :param map: a string representation of the 2d-board
+    :param n: # of mines on the gameboard
+    :return: a string representation of the solved 2d-board or a single '?' if board is unsolvable.
+    """
+
+    def open_zeros(display: bool = False) -> None:
+        """
+        Unveil hints of squares neighboring a square with no surrounding mines.
+
+        :param display: Prints board state after execution if True
+        :return: None
+        """
+
+        if display:
+            print(hashmaptostring(lookup, len(board_2d), len(board_2d[0])), '\n')
         for pos, space in lookup.items():
             if space.hint == '0':
                 for neighbor in space.neighbors.values():
@@ -60,8 +99,14 @@ def solve_mine(map, n):
             print(hashmaptostring(lookup, len(board_2d), len(board_2d[0])))
             print()
 
-    def mark_spaces(display=False):
-        """Mark discernible mines"""
+    def mark_spaces(display: bool = False) -> int:
+        """
+        Deduce which squares are holding mines based off visible hints & mark them w/ an 'x'.
+
+        :param display: Prints board state after execution if True
+        :return: # of mines found during this invocation
+        """
+
         nfound = 0
         for space in lookup.values():
             if space.hint.isnumeric():
@@ -77,8 +122,14 @@ def solve_mine(map, n):
             print()
         return nfound
 
-    def open_safe_spaces(display=False):
-        """Open discernible safe spaces"""
+    def open_safe_spaces(display: bool = False) -> bool:
+        """
+        Deduce which squares ARE NOT holding mines based off visible hints & 'open' them.
+
+        :param display: Prints board state after execution if True
+        :return: True if board state was altered during this invocation
+        """
+
         space_unpacked = False
         for space in lookup.values():
             if space.hint.isnumeric():
@@ -94,8 +145,13 @@ def solve_mine(map, n):
             print()
         return space_unpacked
 
-    def find_safe_spaces(display=False):
-        """Find & open additional safe spaces using set operations"""
+    def find_safe_spaces(display: bool = False) -> bool:
+        """
+        Use set operations to further deduce which squares ARE NOT holding mines based off visible hints & 'open' them.
+
+        :param display: Prints board state after execution if True
+        :return: True if board state was altered during this invocation
+        """
 
         updated = False
 
@@ -133,6 +189,7 @@ def solve_mine(map, n):
                         for pos in new_zone:
                             lookup[pos].hint = f'{open(*pos)}'
                             updated = True
+
         if display and updated:
             print(hashmaptostring(lookup, len(board_2d), len(board_2d[0])))
             print()
